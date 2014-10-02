c22e80a8f6a9f0038c440947b57abed9a4ec845d
Robert Pieczonka <rpieczonka@advaoptical.com>
2014-10-02 09:26:15 +0200

    switch to custimized swagger-ui 2.1.0

--- swagger-doclet/src/main/java/com/carma/swagger/doclet/parser/JaxRsAnnotationParser.java
+++ swagger-doclet/src/main/java/com/carma/swagger/doclet/parser/JaxRsAnnotationParser.java
@@ -32,7 +32,7 @@ import com.sun.javadoc.RootDoc;
 public class JaxRsAnnotationParser {
 
 	private static final String SWAGGER_VERSION = "1.2";
-	private static final String SWAGGER_UI_VERSION = "2.0.23";
+	private static final String SWAGGER_UI_VERSION = "2.1.0";
 
 	private final DocletOptions options;
 	private final RootDoc rootDoc;
